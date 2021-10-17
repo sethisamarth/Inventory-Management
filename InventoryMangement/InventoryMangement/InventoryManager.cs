15c6641338de8e17ffe4fc530a742ab52d3198de
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-17 13:39:15 +0530

    Access to inventory

---
+++ InventoryMangement/InventoryMangement/InventoryManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryMangement
+{
+    class InventoryManager
+    {
+        public void DisplayInventory(List<InventoryUtility.Rice> ricelist)
+        {
+            foreach (InventoryUtility.Rice i in ricelist)
+            {
+                Console.WriteLine("Name: {0} \nKg: {1} \nPrice: {2} \nType:{3}\n", i.Name, i.Kg, i.Price, i.Type);
+            }
+
+        }
+        public void DisplayInventory(List<InventoryUtility.Wheat> wheatList)
+        {
+            foreach (InventoryUtility.Wheat i in wheatList)
+            {
+                Console.WriteLine("Name: {0} \nKg: {1} \nPrice: {2} \nType:{3}\n", i.Name, i.Kg, i.Price, i.Type);
+            }
+        }
+        public void DisplayInventory(List<InventoryUtility.Pulse> pulseList)
+        {
+            foreach (InventoryUtility.Pulse i in pulseList)
+            {
+                Console.WriteLine("Name: {0} \nKg: {1} \nPrice: {2} \nType:{3}\n", i.Name, i.Kg, i.Price, i.Type);
+            }
+        }
+    }
+}
--- InventoryMangement/InventoryMangement/Program.cs
+++ InventoryMangement/InventoryMangement/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 
 namespace InventoryMangement
 {
@@ -6,7 +8,25 @@ namespace InventoryMangement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Inventory");
+            Console.WriteLine("Welcome to inventory");
+            InventoryManager manager = new InventoryManager();
+            string file = @"C:\Users\Lenovo\Desktop\Inventory Management Application\InventoryMangement\InventoryMangement\json1.json";
+            InventoryUtility utility = JsonConvert.DeserializeObject<InventoryUtility>(File.ReadAllText(file));
+            Console.WriteLine("Display inventory Press 1-Rice Inventory 2-Wheat Inventory 3-Pulse Inventory ");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    manager.DisplayInventory(utility.RiceList);
+                    break;
+                case 2:
+                    manager.DisplayInventory(utility.WheatList);
+                    break;
+                case 3:
+                    manager.DisplayInventory(utility.PulseList);
+                    break;
+            }
         }
     }
 }
