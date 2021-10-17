0822a88724a153282b1ed3aff210862a706bce9c
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-17 13:08:12 +0530

    Access Inventory

---
+++ InventoryManagement/InventoryManagement/InventoryManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagement
+{
+    class InventoryManager
+    {
+        public void DisplayInventory(List<InventoryUtility.Rice> Ricelist)
+        {
+            foreach (InventoryUtility.Rice i in Ricelist)
+            {
+                Console.WriteLine("Name: {0} \nKg: {1} \nPrice: {2} \nType:{3}\n", i.Name, i.Kg, i.Price, i.Type);
+            }
+
+        }
+        public void DisplayInventory(List<InventoryUtility.Wheat> WheatList)
+        {
+            foreach (InventoryUtility.Wheat i in WheatList)
+            {
+                Console.WriteLine("Name: {0} \nKg: {1} \nPrice: {2} \nType:{3}\n", i.Name, i.Kg, i.Price, i.Type);
+            }
+        }
+        public void DisplayInventory(List<InventoryUtility.Pulse> PulseList)
+        {
+            foreach (InventoryUtility.Pulse i in PulseList)
+            {
+                Console.WriteLine("Name: {0} \nKg: {1} \nPrice: {2} \nType:{3}\n", i.Name, i.Kg, i.Price, i.Type);
+            }
+        }
+    }
+}
